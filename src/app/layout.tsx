5e9922632e88ff592411c638af3eab0f9f877fd3
Trần Đức Bo <94088465+baotran123456@users.noreply.github.com>
2023-11-22 14:24:17 +0700

    update

---
+++ src/app/layout.tsx
@@ -0,0 +1,22 @@
+
+import AppHeader from "@/components/header/app.header";
+import ThemeRegistry from "@/components/theme-registry/theme.registry";
+
+const DRAWER_WIDTH = 240;
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>
+        <ThemeRegistry>
+          <AppHeader />
+          {children}
+        </ThemeRegistry>
+      </body>
+    </html>
+  );
+}
